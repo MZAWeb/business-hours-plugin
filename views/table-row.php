ced5837cef1d3e8346fc54892936b48dd32d0108
Daniel Dvorkin <daniel@mzaweb.com>
2012-11-22 17:06:37 -0300

    Made table (for shortcodes and/or widget) templetable
    Closes #2

---
+++ views/table-row.php
@@ -0,0 +1,9 @@
+<tr>
+	<td class='business_hours_table_day'><?php echo ucwords( $day_name );?></td>
+	<?php if ( $is_open_today ) { ?>
+		<td class='business_hours_table_open'><?php echo ucwords( $open );?></td>
+		<td class='business_hours_table_close'><?php echo ucwords( $close );?></td>
+	<?php } else { ?>
+		<td class='business_hours_table_closed' colspan='2' align='center'><?php echo $closed_text; ?></td>
+<?php } ?>
+</tr>
