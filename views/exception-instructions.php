7e22a07825a190daa926fd454c55b7d05a3076cd
MZAWeb <daniel@mzaweb.com>
2012-11-25 05:55:40 -0300

    Make the exceptions instructions an overrideable view so users can remove them
    Updates #3

---
+++ views/exception-instructions.php
@@ -0,0 +1,11 @@
+<?php _e( "<b>Instructions:</b>", 'business-hours' );?>
+<br/>
+<?php _e( '1) Click on "Add Exception".', 'business-hours' ); ?>
+<br/>
+<?php _e( "2) Select a day, month and / or year (i.e. To add an exception for every March 1st select day 1, month March and leave the year empty).", 'business-hours' ); ?>
+<br/>
+<?php _e( "3) Type the open and close hours for this exception. Leave empty if your business remains closed during this exception.", 'business-hours' ); ?>
+<br/>
+<?php _e( "4) If you want to add more exceptions, click the 'Add Exceptions' button and repeat this process in the new added row.", 'business-hours' ); ?>
+<br/>
+<?php _e( "5) Need help setting exceptions? <a href='https://github.com/MZAWeb/business-hours-plugin/issues'>Open a ticket in GitHub</a>", 'business-hours' ); ?>
