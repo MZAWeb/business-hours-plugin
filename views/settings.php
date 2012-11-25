17513ab7c179e53f5ea448f29a5246d8ef00b3ef
MZAWeb <daniel@mzaweb.com>
2012-11-25 18:19:58 -0300

    Improve settings page architecture. Use filters/actions for sections.
    Updates #7

--- views/settings.php
+++ views/settings.php
@@ -11,56 +11,8 @@
 
 			<?php wp_nonce_field( BusinessHours::SLUG, 'bh_nonce' ); ?>
 
-			<table class="form-table">
-				<tr>
-					<td>
-						<div>
-							<h3><?php _e( 'Business hours for each day of the week', 'business-hours' );?></h3>
+			<?php do_action( 'business-hours-settings-page' ) ?>
 
-							<p><?php _e( "Leave the fields empty for the days your business is closed.", 'business-hours' );?></p>
-						</div>
-						<div>
-							<table id='business_hours_days_table'>
-								<thead>
-								<tr>
-									<td><?php _e( 'Day', 'business-hours' ) ?></td>
-									<td><?php _e( 'Open', 'business-hours' ) ?></td>
-									<td><?php _e( 'Close', 'business-hours' ) ?></td>
-								</tr>
-								</thead>
-								<tbody>
-								<?php $this->_show_days_controls(); ?>
-								</tbody>
-							</table>
-						</div>
-					</td>
-				</tr>
-			</table>
-			<table class="form-table">
-				<tr>
-					<td>
-						<div>
-							<h3><?php _e( 'Exceptions', 'business-hours' );?></h3>
-
-							<p>
-								<?php _e( "Exceptions allow you to set different business hours for specific days (ie: Holidays, Vacations, etc).", 'business-hours' );?>
-								<br/>
-								<?php $this->_show_exceptions_instructions(); ?>
-
-							</p>
-						</div>
-						<div id="exceptions_wrapper">
-							<?php $this->_show_exceptions(); ?>
-						</div>
-						<label for="exception_add">&nbsp;
-							<input type="button" name="exception_add" class="bh-button" id="exception_add"
-							       value="<?php _e( 'Add exception', 'business-hours' );?>"/>
-						</label>
-
-						<div class="bh_clear"></div>
-					</td>
-				</tr>
-			</table>
 			<table class="form-table">
 				<tr>
 					<td>
