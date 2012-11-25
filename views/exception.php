309869254437a44be562cd0e33e3efad9d184e49
MZAWeb <daniel@mzaweb.com>
2012-11-25 05:50:15 -0300

    More work over exceptions admin UI
    Updates #3

---
+++ views/exception.php
@@ -0,0 +1,35 @@
+<div class="exception_date" id="exception_0">
+	<label for="exception_day"><?php _e( 'Day:', 'business-hours' );?>
+		<select name="exception_day" id='exception_day'>
+			<?php $this->_show_exception_days(); ?>
+		</select>
+	</label>
+	<label for="exception_month"><?php _e( 'Month:', 'business-hours' );?>
+		<select name="exception_month" id='exception_month'>
+			<?php $this->_show_exception_months(); ?>
+		</select>
+	</label>
+	<label for="exception_month"><?php _e( 'Year:', 'business-hours' );?>
+		<select name="exception_year" id='exception_year'>
+			<?php $this->_show_exception_years(); ?>
+		</select>
+	</label>
+	<label for="exception_open"><?php _e( 'Open:', 'business-hours' );?>
+		<input name="exception_open" id="exception_open"
+		       class="business_hours_exception_hour_field" type="text" value=""/>
+	</label>
+
+	<label for="exception_close"><?php _e( 'Close:', 'business-hours' );?>
+		<input name="exception_close" id="exception_close"
+		       class="business_hours_exception_hour_field" type="text" value=""/>
+	</label>
+
+	<label class="exception_remove_label" for="exception_remove">&nbsp;
+		<input type="button" name="exception_remove" class="exception_remove bh-button"
+		       data-id='1' value="<?php _e( 'Remove', 'business-hours' );?>"/>
+	</label>
+
+	<input type="hidden" name="exception_number" class="exception_number" value="0"/>
+
+	<div class="bh_clear"></div>
+</div>
