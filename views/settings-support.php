c39d0e23671c9eaeb4e5182e5da9d698c9f22253
MZAWeb <daniel@mzaweb.com>
2012-12-21 16:10:42 -0300

    Tag support links with analyitics tracking
    Fixes #17

--- views/settings-support.php
+++ views/settings-support.php
@@ -5,9 +5,10 @@
 		<li><hr/></li>
 		<li><?php _e( 'Need support? Have an idea? Create a <a href="https://github.com/MZAWeb/business-hours-plugin/issues">new issue in GitHub</a> or in the <a href="http://wordpress.org/support/plugin/business-hours-plugin">support forums</a> at WordPress.org', 'business-hours' ); ?></li>
 		<li><hr/></li>
-		<li><?php _e( '<a href="https://github.com/MZAWeb/business-hours-plugin/wiki">Documentation</a>', 'business-hours' ); ?></li>			<li><?php _e( '<a href="http://twitter.com/MZAWeb">Tweet to me</a>', 'business-hours' ); ?></li>
-		<li><?php _e( '<a href="http://danieldvork.in">My blog</a>', 'business-hours' ); ?></li>
-		<li><?php _e( '<a href="http://mzaweb.com">MZAWeb - WordPress Development</a>', 'business-hours' ); ?></li>
+		<li><?php _e( '<a href="https://github.com/MZAWeb/business-hours-plugin/wiki">Documentation</a>', 'business-hours' ); ?></li>
+		<li><?php _e( '<a href="http://twitter.com/MZAWeb">Tweet to me</a>', 'business-hours' ); ?></li>
+		<li><?php _e( '<a href="http://danieldvork.in/?utm_source=business_hours&utm_medium=settings&utm_campaign=plugin">My blog</a>', 'business-hours' ); ?></li>
+		<li><?php _e( '<a href="http://mzaweb.com/?utm_source=business_hours&utm_medium=settings&utm_campaign=plugin">MZAWeb - WordPress Development</a>', 'business-hours' ); ?></li>
 		<li><hr/></li>
 		<li><?php _e( 'I promise to keep this plugin free for ever. If it makes you happy and you want to make me happy, you can donate a few bucks to me via PayPal at daniel@mzaweb.com', 'business-hours' ); ?></li>
 	</ul>
